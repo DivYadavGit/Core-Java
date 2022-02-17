b5c6fdc7f0d37f7f306f341ed6b3f5de15be590e
Divya <yadavdivya1272@gmail.com>
2022-02-17 23:19:22 +0530

    Pattern Design Program.

---
+++ Patterns/src/com/patterns/printdemo/HalfPyramid.java
@@ -0,0 +1,17 @@
+package com.patterns.printdemo;
+
+public class HalfPyramid {
+
+	public static void main(String[] args) {
+		// Here, n means no. of rows
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
