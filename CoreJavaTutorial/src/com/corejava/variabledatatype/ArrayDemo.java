674267bf99634e1b536e0ff69dcad56da6508579
DivYadavGit <yadavdivya1272@gmail.com>
2022-02-19 08:03:12 -0800

    Code Updated.

--- CoreJavaTutorial/src/com/corejava/variabledatatype/ArrayDemo.java
+++ CoreJavaTutorial/src/com/corejava/variabledatatype/ArrayDemo.java
@@ -3,16 +3,20 @@ package com.corejava.variabledatatype;
 public class ArrayDemo {
 
 	public static void main(String[] args) {
-		int[] arr = {5,6,8,1,3};
-		int element = 1;
-		
-		//Search element's index
-		for(int i = 0; i<arr.length; i++)
-		{
-			if(arr[i] == element)
-				System.out.println("Index of element "+ element+" is "+i);
+		int[] arr = { 5, 6, 8, 1, 3 };
+		int element = 5, index = 0;
+		boolean isFind = false;
+		// Search element's index
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == element) {
+				isFind = true;
+				index = i;
+			}
 		}
-
+		if (isFind)
+			System.out.println("Index of element " + element + " is " + index);
+		else
+			System.out.println("No element found!!");
 	}
 
 }
